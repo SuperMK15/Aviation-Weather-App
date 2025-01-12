a435fba7baf29a1f317441bf9731e0b538e41e4b
Manasva Katyal <78823812+SuperMK15@users.noreply.github.com>
2025-01-12 16:57:42 -0500

    Add comments to code

--- src/weatherApi.js
+++ src/weatherApi.js
@@ -1,10 +1,15 @@
+// Import the axios library, which allows us to make HTTP requests.
 import axios from 'axios';
 
+// Define an asynchronous function that fetches METAR data for a given ICAO code and API key.
 export async function getMETAR(icao, apiKey) {
+  // Make a GET request to the CheckWX API, passing the ICAO code and API key in the headers.
   const response = await axios.get(`https://api.checkwx.com/metar/${icao}/decoded`, {
     headers: { 'X-API-Key': apiKey }
   });
 
+  // Extract the first data object from the response.
   const result = response.data.data[0];
+  // Return the extracted data object.
   return result;
 }
